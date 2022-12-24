3088cc9957f22b649a0b4718ddbca6d184b8d7ed
lochungtin <lochungtin@gmail.com>
2022-12-24 01:32:36 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import Login from './components/login';
+import image from './components/placeholder';
 import add from './icons/add.svg';
 import random from './icons/dice.svg';
 
+import { addEntry, bindEntryData, getImage } from './api/firebase';
 import './App.css';
-
-const image =
-	'iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAYAAADimHc4AAAACXBIWXMAAAsTAAALEwEAmpwYAAADXElEQVR4nO2cz2sTQRTHB1FQPIlXRfBSxYNe/BM8+j8I+gf4J+hNrNV/QMi5Uuip9diTgpB2Jtn3Ih405E1UxB9oLR6ssDIJCk2TzWZ30tnZfD/wToFd9n1m5s0k2acUAAAAAAAAAAAAAAAAAADACNvd5LIRfmwskRbeM5bTRQ4tvKctJdrySvt9+9LcBkyz2TxhhJ4Y4f3QD22qGsL7TsRWunXce/K15efBH9DGEVpo06uEwcivwIOZiMLNBJ9rPpYdO6ME4f1W//USRr8NOgselRdgiUJPZxNpuN1RaQFa+GfoBzGRhhba9TADsm+iFhwz7/xAQDYQEBgICAwEBAYCAgMBgYGAwNRSQNJPzmvhNXeQcWEsrXv5XmUO1E5A4pJv+euhU6Xlb+4zVTFqJ0ALr028n/AzVTFqKIB2J9+PfqiKsWgCvquKUTsBxtL65CWIVlXFqJ2AVv/1kiu4h4qw0JeWbZ1TFaN2Ahxut+MKrlvzByG0WsXk11ZATEBAYCAgMBAQGAjIgbadu7pHt9QcgIApmH5y0wj9GfyJtsdXlGcgIIMdSa6P/IObzUdzWnkEAibQ7rUvauFPYxLUUB6BgDG86nfOaqE3k5Ljsx5AwAgvRU4Z4RdZifFZD2ohoCWdO0b4QdnrpGl6LPP3hIPhpR5EL2DHJleNpV/DbzvLSSjwHkNjoQW0e+0z2vLbA9csKMHt9WdMvpd6EK2A4XJBm2OvO6OEf3v9QgJK1oNoBWjL9zOvnVPCmL1+kShcD6IUsNPr3Mg1YiVbQsZev0g0FkLA9ge+YIQ+506MjJcwba9/VPUgKgHdbvekttScOTlyUEKevf5R1YOoBGhLTwsnSIYSZtzrz70eRCPACN8uPUIt3zuid5YbtRKg+3Tt/2ErktA560HlBQwPW/QudELNnOpBpQVkHrbiCJ5WDyotYOphy0YRjWgFLAIGAsICAYGBgMBAQGAgIDAQEBgIqLuA7He2ECYzBx5eKkTLMi480LSldvkZYHkFI50LSqCHpQW4drxoW8lFBPz21l4Bs4CLLD/LyheuDa8R2sBSxPkECG147x/tLjiYCeiim2Y376Zl78kfrQmuI+ygZTt6iqYuB8P29bRc1ZY6AAAAAAAAAAAAAAAAAABQQfkLFj2aNFFQNUoAAAAASUVORK5CYII=';
 
 const localeCapitalise = (text) => text.charAt(0).toUpperCase() + text.slice(1);
 
@@ -26,11 +25,19 @@ const selectRandom = (count) => {
 const ingredientHandler = (text, index, update, setUpdate) => {
 	if (text.length > 0 && text[text.length - 1] === ' ') {
 		if (update[index] && update[update.length - 1]) update.push('');
-	} else if (text.length === 0) {
+	} else if (text.length === 0 && update.length > 1) {
 		update.splice(index, 1);
 	} else update[index] = text;
 
 	setUpdate(update);
+};
+
+const save = (name, ingredient, image, reset) => {
+	if (!name || ingredient.length === 0) return;
+
+	const ingredients = {};
+	ingredient.forEach((val, index) => (ingredients[index] = val.toLowerCase()));
+	addEntry({ name: name.toLowerCase(), ingredients }, image).then(reset);
 };
 
 const imageHandler = (event, setPreview) => {
@@ -41,14 +48,24 @@ const imageHandler = (event, setPreview) => {
 
 const Card = ({ data, selected }) => {
 	const { key, name, ingredients } = data;
+	const [preview, setPreview] = useState(image);
+	const [loaded, setLoaded] = useState(false);
+
+	if (!loaded) {
+		getImage(key).then((val) => {
+			setPreview(val.data);
+		});
+		setLoaded(true);
+	}
+
 	return (
 		<div className={'cardRoot ' + (selected ? 'cardSelected' : '')}>
 			<p className='cardName'>{allLocaleCapitalise(name)}</p>
-			<img src={`data:image/jpeg;base64,${image}`} alt='img' />
+			<img className='foodPic' src={`data:image/jpeg;base64,${preview}`} alt='img' />
 			<p>Ingredients</p>
 			<div className='ingredients'>
-				{spltHalf([...ingredients]).map((splt) => (
-					<div className='ingredientList'>
+				{spltHalf([...ingredients]).map((splt, sIndex) => (
+					<div className='ingredientList' key={sIndex}>
 						{splt.map((name, index) => (
 							<div className='ingredientItem' key={index}>
 								<div className='ingredientDot' />
@@ -64,17 +81,32 @@ const Card = ({ data, selected }) => {
 
 const App = () => {
 	const [loggedIn, setLoggedIn] = useState(false);
+
 	const [addBoxOut, setToggleAdd] = useState(false);
+
 	const [newDishName, setNewDishName] = useState('');
-	const [preview, setPreview] = useState(image);
 	const [newIngredientList, setNewIngredientList] = useState(['']);
-	const [filters, setFilters] = useState('');
+	const [preview, setPreview] = useState(image);
+
 	const [selection, setSelection] = useState([-1, -1]);
 
-	let data = [
-		{ name: 'creamy mushroom chicken', ingredients: ['cream', 'mushroom', 'chicken'] },
-		{ name: 'tomato meatballs', ingredients: ['meatballs'] },
-	];
+	const [filters, setFilters] = useState('');
+	const [bound, setBound] = useState(false);
+	const [data, setData] = useState([]);
+
+	if (!bound) {
+		bindEntryData(setData);
+		setBound(true);
+	}
+
+	let display = data;
+	if (filters !== 'all' && filters !== '*') {
+		const fltr = filters.split(' ');
+		display = data.filter((entry) => {
+			if (fltr.length !== entry.ingredients.size) return false;
+			return fltr.every((word) => entry.ingredients.has(word));
+		});
+	}
 
 	if (loggedIn)
 		return (
@@ -105,6 +137,7 @@ const App = () => {
 								type='text'
 								placeholder='Dish name'
 								onChange={(event) => setNewDishName(event.target.value)}
+								value={allLocaleCapitalise(newDishName)}
 							/>
 							<div className='ingredientList'>
 								{newIngredientList.map((name, index) => (
@@ -126,6 +159,17 @@ const App = () => {
 									</div>
 								))}
 							</div>
+							<button
+								className='saveButton'
+								onClick={(event) =>
+									save(newDishName, newIngredientList, preview, () => {
+										setNewIngredientList(['']);
+										setPreview(image);
+										setNewDishName('');
+									})
+								}>
+								<p>SAVE</p>
+							</button>
 						</div>
 						<div className='addImageSide'>
 							<img className='imagePreview' src={`data:image/jpeg;base64,${preview}`} alt='img' />
@@ -142,8 +186,8 @@ const App = () => {
 				)}
 				{filters !== '' ? (
 					<div className='recipes'>
-						{spltHalf(data).map((splt, lrIndex) => (
-							<div className='list'>
+						{spltHalf(display).map((splt, lrIndex) => (
+							<div className='list' key={lrIndex}>
 								{splt.map((data, index) => (
 									<Card
 										data={data}
--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,10 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { get, getDatabase, onValue, ref, set } from 'firebase/database';
 
 const app = initializeApp({
 	apiKey: 'AIzaSyBQFgJ-q7bszU0OPREzr2nxHvrnaC09YH8',
 	authDomain: 'munch-62903.firebaseapp.com',
+	databaseURL: 'https://munch-62903-default-rtdb.europe-west1.firebasedatabase.app/',
 	projectId: 'munch-62903',
 	storageBucket: 'munch-62903.appspot.com',
 	messagingSenderId: '1023096051872',
@@ -25,17 +26,31 @@ export const loginWPswd = (pswd, onComplete) => {
 };
 
 export const bindEntryData = (setState) => {
-	onValue(ref(db, 'entryData'), (snapshot) => {
+	onValue(ref(db, 'recipe'), (snapshot) => {
 		let data = snapshot.val();
-		setState(data !== null ? data : {});
+		setState(
+			data !== null
+				? Object.entries(data).map(([k, v]) => ({
+						key: v.key,
+						name: v.name,
+						ingredients: new Set(Object.entries(v.ingredients).map(([ki, vi]) => vi)),
+				  }))
+				: [],
+		);
 	});
 };
 
-export const addEntry = (entry) => {
+export const getImage = (key) => get(ref(db, `image/${key}`)).then((val) => val.val());
+
+export const addEntry = (data, image) => {
 	const key = new Date().getTime();
-	set(ref(db, `entryData/${key}`), { ...entry, key });
+	return Promise.all([
+		set(ref(db, `recipe/${key}`), { ...data, key }),
+		set(ref(db, `image/${key}`), { data: image }),
+	]);
 };
 
-export const removeEntry = (entry) => {
-	set(ref(db, `entryData/${entry.key}`), null);
+export const removeEntry = (key) => {
+	set(ref(db, `recipe/${key}`), null);
+	set(ref(db, `image/${key}`), null);
 };
